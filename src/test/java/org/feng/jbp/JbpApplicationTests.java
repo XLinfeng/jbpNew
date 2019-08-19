8d8f09a5d2454d917c46d5822eefc3b66daf7e30
apple <xianglf1983@163.com>
2019-08-19 21:10:29 +0800

    Initial commit

---
+++ src/test/java/org/feng/jbp/JbpApplicationTests.java
@@ -0,0 +1,16 @@
+package org.feng.jbp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JbpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
